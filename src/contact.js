5b81cac62e174ab5e9e0dc3ed0e7434cece9bd43
teaigwe~ <tonyesther60@gmail.com>
2024-06-04 23:45:12 +0100

    done with the error state

--- src/contact.js
+++ src/contact.js
@@ -1,10 +1,25 @@
-let firstName = document.body.getElementsByClassName('.firstname');
-let lastName = document.body.getElementsByClassName('.lastname');
+/* let firstName = document.body.getElementsByClassName('firstname').value4;
+let lastName = document.body.getElementsByClassName('.lastname'); */
 
 let submitBtn = document.body.getElementsByTagName('button')[0];
 
-submitBtn.addEventListener('click',()=>{
-    if (firstName.addEventListener === ""){
-        firstName.className = 'redinput'
-    }
+
+const requiredFields = [
+    firstName = document.body.querySelector('.firstname'),
+    lastName = document.body.querySelector('.lastname'),
+    email = document.body.querySelector('.email'),
+    msgbox = document.body.querySelector('.msgbox'),
+]
+
+requiredFields.forEach(fields => {
+    fields.classList.add('h4')
 })
+submitBtn.addEventListener('click', ()=>{
+
+ requiredFields.forEach(fields =>{
+if (fields.value.trim()  ===""){
+fields.classList.add('redinput')
+} else {fields.classList.remove('redinput')}
+ })
+})
+
