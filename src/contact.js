70ba0cfbc327ced9eef2e020d36bf61f751aa282
teaigwe~ <tonyesther60@gmail.com>
2024-06-05 23:07:12 +0100

    error handling

--- src/contact.js
+++ src/contact.js
@@ -1,25 +1,47 @@
 /* let firstName = document.body.getElementsByClassName('firstname').value4;
 let lastName = document.body.getElementsByClassName('.lastname'); */
 
-let submitBtn = document.body.getElementsByTagName('button')[0];
+ let submitBtn = document.body.getElementsByTagName('button')[0];
 
-
-const requiredFields = [
-    firstName = document.body.querySelector('.firstname'),
-    lastName = document.body.querySelector('.lastname'),
-    email = document.body.querySelector('.email'),
-    msgbox = document.body.querySelector('.msgbox'),
+let requiredFields = [
+{
+    input:document.body.querySelector('.firstname'),
+error: document.body.querySelector('.errtxt[data-for="firstname"]')
+},
+ {
+    input: document.body.querySelector('.lastname'), 
+ error: document.body.querySelector('.errtxt[data-for="lastname"]')
+},
+ {
+    input: document.body.querySelector('.email'), 
+ error: document.body.querySelector('.errtxt[data-for="email"]')
+},
+/*  {
+    input: document.body.querySelector('.query'), 
+ error: document.body.querySelector('.errtxt[data-for="query"]')
+}, */
+ {
+    input: document.body.querySelector('.msgbox'),
+ error:document.body.querySelector('.errtxt[data-for="msgbox"]')
+},
 ]
 
+
 requiredFields.forEach(fields => {
-    fields.classList.add('h4')
+    fields.input.classList.add('inputfont')
 })
+
 submitBtn.addEventListener('click', ()=>{
 
  requiredFields.forEach(fields =>{
-if (fields.value.trim()  ===""){
-fields.classList.add('redinput')
-} else {fields.classList.remove('redinput')}
+if (fields.input.value.trim()  ===""){
+fields.input.classList.add('errbdr');
+fields.error.style.display = 'block'
+} else {
+fields.input.classList.remove('errbdr');
+fields.error.style.display = 'none'
+}
  })
+
 })
 
